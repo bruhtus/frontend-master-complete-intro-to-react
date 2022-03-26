1f799f6218fafea446000f42b7938e1b67d52ae9
bruhtus <hibruhtus@gmail.com>
2022-03-27 05:28:27 +0700

    feat(hooks): add example of react hooks

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,22 +1,54 @@
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, props.name),
-    React.createElement('h3', {}, props.animal),
-    React.createElement('h3', {}, props.breed),
-  ]);
-};
+// import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Pet from './Pet';
+import SearchParams from './SearchParams';
+
+// const App = () => {
+//   return React.createElement(
+//     'div', {},
+//     [
+//       React.createElement('h1', {}, 'adopt me!'),
+//       React.createElement(Pet, {
+//         name: 'Luna',
+//         animal: 'Dog',
+//         breed: 'Havanese'
+//       }),
+//       React.createElement(Pet, {
+//         name: 'Pepper',
+//         animal: 'Bird',
+//         breed: 'Cockatiel'
+//       }),
+//       React.createElement(Pet, {
+//         name: 'Sudo',
+//         animal: 'Dog',
+//         breed: 'Wheaten Terrier'
+//       }),
+//     ]
+//   );
+// };
+
+// JSX example
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>adopt me!</h1>
+//       <Pet name='Luna' animal='Dog' breed='Havanese' />
+//       <Pet name='Pepper' animal='Bird' breed='Cockatiel' />
+//       <Pet name='Sudo' animal='Dog' breed='Wheaten Terrier' />
+//     </div>
+//   );
+// };
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      React.createElement('h1', {}, 'adopt me!'),
-      React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanese' }),
-      React.createElement(Pet, { name: 'Pepper', animal: 'Bird', breed: 'Cockatiel' }),
-      React.createElement(Pet, { name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier' }),
-    ]
+  return (
+    <div>
+      <h1>adopt me!</h1>
+      <SearchParams />
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+// ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
