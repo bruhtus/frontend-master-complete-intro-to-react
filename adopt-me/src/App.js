e44f86fae3813982cffa4058b1b1967a49bcf236
bruhtus <hibruhtus@gmail.com>
2022-03-27 17:13:58 +0700

    feat(details): add photos in the details page

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,8 +1,10 @@
 // import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 // import Pet from './Pet';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 // const App = () => {
 //   return React.createElement(
@@ -44,8 +46,21 @@ import SearchParams from './SearchParams';
 const App = () => {
   return (
     <div>
-      <h1>adopt me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>adopt me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,4 +1,5 @@
 // import React from 'react';
+import { Link } from 'react-router-dom';
 
 // const Pet = (props) => {
 //   return React.createElement('div', {}, [
@@ -19,7 +20,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -27,7 +28,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
